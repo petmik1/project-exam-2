918f006b714c755c8b97a23fc4de4d728cf7ce03
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-08-17 14:52:36 +0200

    added unit and e2e test plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,25 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  "overrides": [
+    {
+      "files": ['**/*.test.jsx', '**/*.test.js'],
+      "env": { "jest": true },
+      "plugins": ["jest"],
+      "extends": ["plugin:jest/recommended"],
+      "rules": { "jest/prefer-expect-assertions": "off" }
+    },
+    {
+      "files": ["**/*.cy.js"],
+      "env": { "cypress/globals": true },
+      "plugins": ["cypress"],
+      "extends": ["plugin:cypress/recommended"],
+      "rules": {
+        "cypress/no-unnecessary-waiting": "off",
+        "no-unused-vars": "off"
+      }
+    }
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
