94cefc09e34251ee76241b655fe1f7a0a05a9940
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-08-18 15:12:47 +0200

    installed mui and started organizing the theme

---
+++ src/theme/theme.jsx
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00679F',
+    },
+    secondary: {
+      main: '#DAE6EF',
+    },
+  },
+  container: {
+    vh: '100vh',
+  },
+})
+
+export default theme
