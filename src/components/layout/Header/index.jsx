67cd607288d89abf15115c3ac1840f512346571b
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-08-22 00:20:04 +0200

    worked on making the navbar responsive

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,7 +1,15 @@
-import { AppBar, Typography, Toolbar, Link } from '@mui/material'
-import IconButton from '@mui/material/IconButton'
+import { AppBar, Typography, Toolbar, Link, Box } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import PermIdentityIcon from '@mui/icons-material/PermIdentity'
+import { useState } from 'react'
 
 function Header() {
+  const [navBarOpen, setNavBarOpen] = useState(false)
+
+  function hamburgerMenu() {
+    setNavBarOpen((prev) => !prev)
+  }
+
   return (
     <AppBar
       position="static"
@@ -14,32 +22,60 @@ function Header() {
     >
       <Toolbar
         sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
           mt: '1rem',
         }}
       >
-        <Link
-          href="/"
-          underline="none"
-          color="textPrimary"
-          sx={{ flexGrow: 1 }}
-        >
-          <IconButton>
-            <img src="/logo.png" alt="" width={'150px'} />
-          </IconButton>
+        <Link href="/" underline="none" color="textPrimary">
+          <img src="/logo.png" alt="" width="100px" />
         </Link>
-        <Link href="/login" underline="none" sx={{}}>
+        <MenuIcon onClick={hamburgerMenu} sx={{ display: { md: 'none' } }} />
+        <Link
+          href="/login"
+          underline="none"
+          sx={{ display: { xs: 'none', md: 'block' } }}
+        >
+          <Box display={'flex'} alignItems={'center'}>
+            <Typography
+              variant="h2"
+              sx={{
+                flexGrow: 1,
+                p: '2rem',
+                color: { xs: 'text.secondary', md: 'text.primary' },
+              }}
+            >
+              Login
+            </Typography>
+            <PermIdentityIcon sx={{ color: 'text.primary' }} />
+          </Box>
+        </Link>
+      </Toolbar>
+
+      <Box sx={{ display: { xs: navBarOpen ? 'block' : 'none', md: 'none' } }}>
+        <Link
+          href="/login"
+          underline="none"
+          sx={{
+            display: { xs: 'flex', md: 'none' },
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
           <Typography
             variant="h2"
             sx={{
-              flexGrow: 1,
-              p: '2rem',
-              color: { xs: 'text.secondary', md: 'text.primary' },
+              display: 'flex',
+              alignItems: 'center',
+              color: 'text.secondary',
             }}
           >
             Login
           </Typography>
+          <PermIdentityIcon sx={{ color: 'text.secondary' }} />
         </Link>
-      </Toolbar>
+      </Box>
     </AppBar>
   )
 }
