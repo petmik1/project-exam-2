ee29eccb6a8d41e2c7696a939b6508f9b801add8
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-09-26 20:42:13 +0200

    added form validation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,35 +1,41 @@
-import { TextField, Button, Stack, Link, Typography } from '@mui/material'
-import { useForm } from 'react-hook-form'
-import setTitle from '../../components/setTitle'
-import api from '../../data/apiBase'
-import storage from '../../storage'
+import { TextField, Button, Stack, Link, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup'; 
+import { yupResolver } from '@hookform/resolvers/yup'; 
+import setTitle from '../../components/setTitle';
+import api from '../../data/apiBase';
+import storage from '../../storage';
+
+const schema = yup.object().shape({
+  email: yup.string().required('Email is required').email('Invalid email').matches("^[a-zA-Z0-9._%+-]+@(noroff\.no|stud\.noroff\.no)$", 'The Email has to be a Noroff email address' ),
+  password: yup.string().required('Password is required'),
+});
 
 function Login() {
-  setTitle('Login')
+  setTitle('Login');
   const form = useForm({
     defaultValues: {
       email: '',
       password: '',
     },
-  })
+    resolver: yupResolver(schema),
+  });
 
-  const { register, handleSubmit } = form
-
-  console.log(storage.load('user'))
+  const { register, handleSubmit, formState: { errors } } = form;
 
   const onsubmit = async (data) => {
     try {
-      const response = await api.post('/auth/login', data)
-      storage.save('user', response.data)
-      storage.save('avatar', response.data.avatar)
-      location.href = '/'
+      const response = await api.post('/auth/login', data);
+      storage.save('user', response.data);
+      storage.save('avatar', response.data.avatar);
+      location.href = '/';
     } catch (error) {
       if (error.response) {
-        console.log(error.response.data.errors[0].message)
+        console.log(error.response.data.errors[0].message);
       } else if (error.request) {
-        console.log(error.request)
+        console.log(error.request);
       } else {
-        console.log('Error', error.message)
+        console.log('Error', error.message);
       }
     }
   }
@@ -60,14 +66,18 @@ function Login() {
             label="Email"
             type="email"
             {...register('email')}
+            error={!!errors.email} 
+            helperText={errors.email?.message} 
           ></TextField>
           <TextField
             label="Password"
             type="password"
             {...register('password')}
+            error={!!errors.password} 
+            helperText={errors.password?.message} 
           ></TextField>
-          <Link data-testId="register-link" color="primary" href="/register">
-            Dont have a user? Register here.
+          <Link data-testid="register-link" color="primary" href="/register">
+            Don't have a user? Register here.
           </Link>
           <Button variant="contained" type="submit">
             Login
@@ -75,7 +85,7 @@ function Login() {
         </Stack>
       </form>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
