14c495e98408b792d185b442ca5690ee70d48cf9
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-09-28 20:28:16 +0200

    worked on the edit form and added validation using yup

--- src/pages/CreateVenue/index.jsx
+++ src/pages/CreateVenue/index.jsx
@@ -68,6 +68,7 @@ function CreateVenue() {
       },
     },
   })
+  
 
   const {
     register,
--- src/pages/EditVenue/index.jsx
+++ src/pages/EditVenue/index.jsx
@@ -10,19 +10,19 @@ import {
 import { useForm } from 'react-hook-form'
 import setTitle from '../../components/setTitle'
 import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
 import api from '../../data/apiBase'
 import { useState } from 'react'
 import storage from '../../storage'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
+
 function EditVenue() {
   setTitle('Edit venue')
 
   const validationSchema = yup.object().shape({
     name: yup.string().required('Name is required'),
-    media: yup.string(),
+    media: yup.url,
     maxGuests: yup.number().positive('Max Guests must be a positive number'),
     rating: yup
       .number()
@@ -31,93 +31,72 @@ function EditVenue() {
       .max(5, 'Rating must be at most 5'),
     price: yup.number().positive('Price must be a positive number'),
     description: yup.string(),
+    meta: yup.object().shape({
     wifi: yup.boolean(),
     parking: yup.boolean(),
     breakfast: yup.boolean(),
     pets: yup.boolean(),
-    address: yup.string(),
-    city: yup.string(),
-    zip: yup.string(),
-    country: yup.string(),
-    continent: yup.string(),
-    longitude: yup.string(),
-    latitude: yup.string(),
+    }),
+    location: yup.object().shape({
+      address: yup.string(),
+      city: yup.string(),
+      zip: yup.string(),
+      country: yup.string(),
+    }),
   })
 
   const { id } = useParams()
-  const [name, setName] = useState('')
-  const [media, setMedia] = useState('')
-  const [maxGuests, setMaxGuests] = useState(Number(0))
-  const [rating, setRating] = useState(Number(0))
-  const [price, setPrice] = useState(Number(0))
-  const [description, setDescription] = useState('')
   const [wifi, setWifi] = useState(false)
   const [parking, setParking] = useState(false)
   const [breakfast, setBreakfast] = useState(false)
   const [pets, setPets] = useState(false)
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [zip, setZip] = useState('')
-  const [country, setCountry] = useState('')
-  const [continent, setContinent] = useState('')
-  const [longitude, setLongitude] = useState('')
-  const [latitude, setLatitude] = useState('')
+
 
   const form = useForm({
     resolver: yupResolver(validationSchema),
-    defaultValues: {
-      name: name,
-      media: media,
-      max_guests: maxGuests,
-      rating: rating,
-      price: price,
-      description: description,
-      wifi: wifi,
-      parking: parking,
-      breakfast: breakfast,
-      pets: pets,
-      address: address,
-      city: city,
-      zip: zip,
-      country: country,
-      continent: continent,
-      longitude: longitude,
-      latitude: latitude,
+    defaultValues: async () => {
+      const response = await api.get('/venues/' + id)
+      try {
+        setWifi(response.data.meta.wifi)
+        setParking(response.data.meta.parking)
+        setBreakfast(response.data.meta.breakfast)
+        setPets(response.data.meta.pets)
+
+        return {
+          name: response.data.name,
+          media: response.data.media,
+          maxGuests: response.data.maxGuests,
+          rating: response.data.rating,
+          price: response.data.price,
+          description: response.data.description,
+          meta: {
+            wifi: response.data.meta.wifi,
+            parking: response.data.meta.parking,
+            breakfast: response.data.meta.breakfast,
+            pets: response.data.meta.pets,
+          },
+          location: {
+            address: response.data.location.address,
+            city: response.data.location.city,
+            zip: response.data.location.zip,
+            country: response.data.location.country,
+          },
+        }
+      } catch (error) {
+        console.log(error)
+      }
     },
   })
 
+  console.log(wifi)
+  
   const {
+    register,
     handleSubmit,
     formState: { errors },
   } = form
 
-  const onsubmit = async () => {
-    // if(errors.length === 0) {
-    const data = {
-      name: name,
-      media: media,
-      maxGuests: maxGuests,
-      rating: rating,
-      price: price,
-      description: description,
-      meta: {
-        wifi: wifi,
-        parking: parking,
-        breakfast: breakfast,
-        pets: pets,
-      },
-      location: {
-        address: address,
-        city: city,
-        zip: zip,
-        country: country,
-        continent: continent,
-        longitude: longitude,
-        latitude: latitude,
-      },
-    }
-
-    console.log(data)
+  const onsubmit = async (data) => {
     const response = await api.put('/venues/' + id, data, {
       headers: {
         Authorization: `Bearer ${storage.load('user').accessToken}`,
@@ -129,35 +108,6 @@ function EditVenue() {
       console.log(error)
     }
   }
-  // }
-
-  useEffect(() => {
-    const fetchVenue = async () => {
-      const response = await api.get('/venues/' + id)
-      try {
-        setName(response.data.name)
-        setMedia(response.data.media)
-        setMaxGuests(response.data.maxGuests)
-        setRating(response.data.rating)
-        setPrice(response.data.price)
-        setDescription(response.data.description)
-        setWifi(response.data.meta.wifi)
-        setParking(response.data.meta.parking)
-        setBreakfast(response.data.meta.breakfast)
-        setPets(response.data.meta.pets)
-        setAddress(response.data.location.address)
-        setCity(response.data.location.city)
-        setZip(response.data.location.zip)
-        setCountry(response.data.location.country)
-        setContinent(response.data.location.continent)
-        setLongitude(response.data.location.longitude)
-        setLatitude(response.data.location.latitude)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchVenue()
-  }, [form, id])
 
   const handleWifiChange = (event) => {
     setWifi(event.target.checked)
@@ -226,8 +176,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                {...register('name')}
                 type="text"
               ></TextField>
               {errors.name && (
@@ -242,8 +191,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={media ? media : ''}
-                onChange={(e) => setMedia([e.target.value])}
+                {...register('media')}
                 type="text"
               ></TextField>
               {errors.media && (
@@ -258,8 +206,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={maxGuests ? maxGuests : ''}
-                onChange={(e) => setMaxGuests(e.target.value)}
+                {...register('maxGuests', { valueAsNumber: true })}
                 type="text"
               ></TextField>
               {errors.maxGuests && (
@@ -274,8 +221,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={rating ? rating : ''}
-                onChange={(e) => setRating(e.target.value)}
+                {...register('rating', { valueAsNumber: true })}
                 type="text"
               ></TextField>
               {errors.rating && (
@@ -302,8 +248,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: { xs: '100%', md: '50%' } }}
-                value={price ? price : ''}
-                onChange={(e) => setPrice(e.target.value)}
+                {...register('price', { valueAsNumber: true })}
                 type="text"
               ></TextField>
               {errors.price && (
@@ -330,8 +275,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: { xs: '100%', md: '50%' } }}
-                value={description ? description : ''}
-                onChange={(e) => setDescription(e.target.value)}
+                {...register('description')}
                 type="text"
               ></TextField>
               {errors.description && (
@@ -349,9 +293,11 @@ function EditVenue() {
               flexDirection={'column'}
             >
               <FormControlLabel
-                control={<Switch checked={wifi} onClick={handleWifiChange} />}
+                control={<Switch checked={wifi} onChange={handleWifiChange} />}
                 label="Wifi"
                 labelPlacement="start"
+                {...register('meta.wifi')}
+                
               />
               {errors.wifi && (
                 <Typography variant="body1" color="error">
@@ -364,6 +310,7 @@ function EditVenue() {
                 }
                 label="Parking"
                 labelPlacement="start"
+                {...register('meta.parking')}
               />
               {errors.parking && (
                 <Typography variant="body1" color="error">
@@ -376,6 +323,7 @@ function EditVenue() {
                 }
                 label="Breakfast"
                 labelPlacement="start"
+                {...register('meta.breakfast')}
               />
               {errors.breakfast && (
                 <Typography variant="body1" color="error">
@@ -386,6 +334,7 @@ function EditVenue() {
                 control={<Switch checked={pets} onClick={handlePetsChange} />}
                 label="Pets"
                 labelPlacement="start"
+                {...register('meta.pets')}
               />
               {errors.pets && (
                 <Typography variant="body1" color="error">
@@ -399,8 +348,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={address ? address : ''}
-                onChange={(e) => setAddress(e.target.value)}
+                {...register('location.address')}
                 type="text"
               ></TextField>
               {errors.address && (
@@ -415,8 +363,7 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={city ? city : ''}
-                onChange={(e) => setCity(e.target.value)}
+                {...register('location.city')}
                 type="text"
               ></TextField>
               {errors.city && (
@@ -432,8 +379,7 @@ function EditVenue() {
 
               <TextField
                 sx={{ width: '100%' }}
-                value={zip ? zip : ''}
-                onChange={(e) => setZip(e.target.value)}
+                {...register('location.zip')}
                 type="text"
               ></TextField>
               {errors.zip && (
@@ -448,73 +394,12 @@ function EditVenue() {
               </Typography>
               <TextField
                 sx={{ width: '100%' }}
-                value={country ? country : ''}
-                onChange={(e) => setCountry(e.target.value)}
+                {...register('location.country')}
                 type="text"
               ></TextField>
               {errors.country && (
                 <Typography variant="body1" color="error">
                   {errors.country.message}
-                </Typography>
-              )}
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography variant="body1" color="initial">
-                Continent
-              </Typography>
-              <TextField
-                sx={{ width: '100%' }}
-                value={continent ? continent : ''}
-                onChange={(e) => setContinent(e.target.value)}
-                type="text"
-              ></TextField>
-              {errors.continent && (
-                <Typography variant="body1" color="error">
-                  {errors.continent.message}
-                </Typography>
-              )}
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography variant="body1" color="initial">
-                Longitude
-              </Typography>
-              <TextField
-                sx={{ width: '100%' }}
-                value={longitude ? longitude : ''}
-                onChange={(e) => setLongitude(e.target.value)}
-                type="text"
-              ></TextField>
-              {errors.longitude && (
-                <Typography variant="body1" color="error">
-                  {errors.longitude.message}
-                </Typography>
-              )}
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              display={'flex'}
-              justifyContent={'center'}
-              alignItems={'center'}
-              flexDirection={'column'}
-            >
-              <Typography
-                variant="body1"
-                color="initial"
-                textAlign={'left'}
-                width={{ xs: '100%', md: '50%' }}
-              >
-                Latitude
-              </Typography>
-              <TextField
-                sx={{ width: '50%' }}
-                value={latitude ? latitude : ''}
-                onChange={(e) => setLatitude(e.target.value)}
-                type="text"
-              ></TextField>
-              {errors.latitude && (
-                <Typography variant="body1" color="error">
-                  {errors.latitude.message}
                 </Typography>
               )}
             </Grid>
