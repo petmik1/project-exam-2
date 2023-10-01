a48671400ea6219b47f8448ac5150cf19dc922eb
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-10-01 16:19:46 +0200

    header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -35,263 +35,48 @@ function Header() {
       <Toolbar
         sx={{
           display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
           justifyContent: 'space-between',
           mt: '1rem',
         }}
       >
-        <Link href="/" underline="none" color="textPrimary">
-          <img src="/logo.png" alt="" width="100px" />
-        </Link>
-        <MenuIcon
-          fontSize="large"
-          onClick={hamburgerMenu}
-          sx={{ display: { md: 'none' } }}
-        />
-
-        <Box display={!user ? { xs: 'none', md: 'block' } : 'none'}>
-          <Box
-            component={NavLink}
-            to="/login"
-            style={({ isActive }) => {
-              return {
-                textDecoration: isActive ? 'underline' : 'none',
-                textDecorationColor: '#101010',
-              }
-            }}
-          >
-            <Typography
-              variant="h2"
-              sx={{
-                flexGrow: 1,
-                p: '2rem',
-                color: { xs: 'text.secondary', md: 'text.primary' },
-              }}
-            >
-              Login
-            </Typography>
-          </Box>
+        <Box
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+          width={'100%'}
+        >
+          <Link href="/" underline="none" color="textPrimary">
+            <img src="/logo.png" alt="" width="100px" />
+          </Link>
+          <MenuIcon
+            fontSize="large"
+            onClick={hamburgerMenu}
+            sx={{ display: { md: 'none' } }}
+          />
         </Box>
-        <Box display={user ? { xs: 'none', md: 'flex' } : 'none'}>
-          <Box
-            component={NavLink}
-            to="/booking"
-            style={({ isActive }) => {
-              return {
-                textDecoration: isActive ? 'underline' : 'none',
-                textDecorationColor: '#101010',
-              }
-            }}
-          >
-            <Typography
-              variant="h2"
-              sx={{
-                flexGrow: 1,
-                p: '2rem',
-                color: { xs: 'text.secondary', md: 'text.primary' },
-              }}
-            >
-              booking
-            </Typography>
+        <Box
+          display={{ xs: navBarOpen ? 'flex' : 'none', md: 'flex' }}
+          flexDirection={{xs:'column', md:'row'}}
+          alignItems={'center'}
+          justifyContent={'center'}
+          width={'100%'}
+        >
+          <Box component={NavLink} display={!user ? 'block' : 'none' }  p={'0 1rem'} to="/login">
+            <Typography color={{xs:'text.secondary', md:'text.primary'}}>login</Typography>
           </Box>
-          <Box
-            component={NavLink}
-            to="/createVenue"
-            style={({ isActive }) => {
-              return {
-                textDecoration: isActive ? 'underline' : 'none',
-                textDecorationColor: '#101010',
-              }
-            }}
-          >
-            <Typography
-              variant="h2"
-              sx={{
-                flexGrow: 1,
-                p: '2rem',
-                color: { xs: 'text.secondary', md: 'text.primary' },
-              }}
-            >
-              create venue
-            </Typography>
+          <Box component={NavLink} display={user ? 'block' : 'none' } p={'0 1rem'} to="/createVenue">
+            <Typography color={{xs:'text.secondary', md:'text.primary'}}>create venue</Typography>
           </Box>
-          <Box
-            component={NavLink}
-            onClick={Logout}
-            sx={{ textDecoration: 'none' }}
-          >
-            <Typography
-              variant="h2"
-              sx={{
-                flexGrow: 1,
-                p: '2rem',
-                color: { xs: 'text.secondary', md: 'text.primary' },
-              }}
-            >
-              logout
-            </Typography>
+          <Box component={NavLink} display={user ? 'flex' : 'none' } p={'0 1rem'} justifyContent={'center'} alignItems={'center'} to="/profile">
+            <Typography color={{xs:'text.secondary', md:'text.primary'}}>profile</Typography>
+            <PermIdentityIcon sx={{xs:'text.secondary', md:'text.primary'}}/>
           </Box>
-          <Box
-            component={NavLink}
-            to="/profile"
-            style={({ isActive }) => {
-              return {
-                textDecoration: isActive ? 'underline' : 'none',
-                textDecorationColor: '#101010',
-              }
-            }}
-          >
-            <Typography
-              variant="h2"
-              display={'flex'}
-              alignItems={'center'}
-              sx={{
-                flexGrow: 1,
-                p: '2rem',
-                color: { xs: 'text.secondary', md: 'text.primary' },
-              }}
-            >
-              profile
-              <PermIdentityIcon sx={{ color: 'text.primary' }} />
-            </Typography>
+          <Box component={NavLink} display={user ? 'block' : 'none' } p={'0 1rem'} onClick={Logout}>
+            <Typography color={{xs:'text.secondary', md:'text.primary'}}>logout</Typography>
           </Box>
         </Box>
       </Toolbar>
-
-      <Box
-        sx={{
-          display: !user
-            ? { xs: navBarOpen ? 'block' : 'none', md: 'none' }
-            : 'none',
-        }}
-      >
-        <Box
-          component={NavLink}
-          to="/login"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : 'none',
-              textDecorationColor: '#ffffff',
-            }
-          }}
-          sx={{
-            display: { xs: 'flex', md: 'none' },
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography
-            variant="h2"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              color: 'text.secondary',
-              textDecorationColor: '#ffffff',
-            }}
-          >
-            Login
-          </Typography>
-          <PermIdentityIcon sx={{ color: 'text.secondary' }} />
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: user
-            ? { xs: navBarOpen ? 'flex' : 'none', md: 'none' }
-            : 'none',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          p: '1rem',
-        }}
-      >
-        <Box
-          component={NavLink}
-          to="/booking"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : 'none',
-              textDecorationColor: '#ffffff',
-            }
-          }}
-        >
-          <Typography
-            variant="h2"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              color: 'text.secondary',
-            }}
-          >
-            booking
-          </Typography>
-        </Box>
-        <Box
-          component={NavLink}
-          to="/createVenue"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : 'none',
-              textDecorationColor: '#ffffff',
-            }
-          }}
-        >
-          <Typography
-            variant="h2"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              color: 'text.secondary',
-            }}
-          >
-            create venue
-          </Typography>
-        </Box>
-        <Box
-          component={NavLink}
-          onClick={Logout}
-          sx={{ textDecoration: 'none' }}
-        >
-          <Typography
-            variant="h2"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              color: 'text.secondary',
-            }}
-          >
-            logout
-          </Typography>
-        </Box>
-        <Box
-          component={NavLink}
-          to="/profile"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : 'none',
-              textDecorationColor: '#ffffff',
-            }
-          }}
-          sx={{
-            display: { xs: 'flex', md: 'none' },
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography
-            variant="h2"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              color: 'text.secondary',
-            }}
-          >
-            profile
-          </Typography>
-          <PermIdentityIcon sx={{ color: 'text.secondary' }} />
-        </Box>
-      </Box>
     </AppBar>
   )
 }
